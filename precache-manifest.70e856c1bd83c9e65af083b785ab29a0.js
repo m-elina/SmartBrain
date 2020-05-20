66aa677c4231e1e032cf97902bc538ea9bc77107
[m-elina] <[melina281.ml@gmail.com]>
2020-05-20 12:09:16 +0200

    Updates

---
+++ precache-manifest.70e856c1bd83c9e65af083b785ab29a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6986b88359510d423cdcacebbebddb",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "524ded5c5661edc11274",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d21a61e8dc5f98b31e2",
+    "url": "/SmartBrain/static/css/main.8edc2faf.chunk.css"
+  },
+  {
+    "revision": "524ded5c5661edc11274",
+    "url": "/SmartBrain/static/js/2.a6586f1a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain/static/js/2.a6586f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d21a61e8dc5f98b31e2",
+    "url": "/SmartBrain/static/js/main.0da66e05.chunk.js"
+  },
+  {
+    "revision": "b88d0d3ac73687a922b1",
+    "url": "/SmartBrain/static/js/runtime-main.c123aeca.js"
+  },
+  {
+    "revision": "ee4f1a563e1e6fe8fd1e233f9792a814",
+    "url": "/SmartBrain/static/media/SmartBrain-logo.ee4f1a56.png"
+  }
+]);
